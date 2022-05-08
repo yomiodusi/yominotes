5580294e8b5ebb3f502e56326c4f3c194139f587
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-05-08 18:41:14 -0400

    functionality complete for adding notes to page

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
+const fs = require("fs")
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+app.use(express.static('public'))
+const parser = bodyParser.urlencoded({extended: true})
+const jsonParser = bodyParser.json()
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'))
@@ -13,6 +18,17 @@ app.get('/notes', (req, res) => {
   
   app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname+'/db/db.json'))
+  })
+
+  app.post('/api/notes',jsonParser,(req,res) =>{
+    let data = req.body
+    let db = fs.readFileSync("db/db.json")
+    let read=JSON.parse(db)
+    read.push(data)
+     let finalJSON = JSON.stringify(read)
+     fs.writeFileSync("db/db.json",finalJSON)
+     res.sendStatus(200)
+    
   })
 
 app.listen(port, () => {
