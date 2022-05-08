438b7ad88470d4808a7deeab668dc94f82232925
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-05-08 16:30:38 -0400

    api json completed

--- Develop/app.js
+++ Develop/app.js
@@ -4,13 +4,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'../../../index.html'))
+  res.sendFile(path.join(__dirname+'/public/index.html'))
 })
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname+'../../../notes.html'))
+    res.sendFile(path.join(__dirname+'/public/notes.html'))
   })
   
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname+'/db/db.json'))
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
