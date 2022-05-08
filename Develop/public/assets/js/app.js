eda071e5040e6bc745b1e892ac1000fdb2dd08a3
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-05-08 16:13:18 -0400

    created express server

---
+++ Develop/public/assets/js/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'../../../index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname+'../../../notes.html'))
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
